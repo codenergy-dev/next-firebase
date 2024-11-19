22ea39fc3c026a6b89c8af0dca6651220a804bb6
Lucas Lannes <lucaslannesr@gmail.com>
2024-11-19 12:56:06 -0300

    Setup Firebase for client-side

---
+++ app/firebase.ts
@@ -0,0 +1,35 @@
+import { getApp, getApps, initializeApp } from "firebase/app"
+import { Auth, connectAuthEmulator, getAuth } from "firebase/auth"
+
+const useEmulator = 
+    typeof window !== 'undefined' &&
+    process.env.NEXT_PUBLIC_FIREBASE_AUTH_EMULATOR_HOST
+
+const firebaseConfig = useEmulator ? {
+    apiKey: 'NEXT_PUBLIC_FIREBASE_API_KEY',
+    authDomain: 'NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN',
+} : {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+}
+
+const firebase =
+    typeof window === 'undefined'
+    ? undefined
+    : getApps().length > 0 ? getApp() : initializeApp(firebaseConfig)
+
+const auth =
+    typeof window === 'undefined'
+    ? {} as Auth
+    : getAuth()
+
+if (useEmulator) connectAuthEmulator(
+    auth, 
+    process.env.NEXT_PUBLIC_FIREBASE_AUTH_EMULATOR_HOST!,
+    { disableWarnings: true },
+)
+
+export { firebase, auth }
